301d9b9e346e6207211f621c41a01c777bd63eca
urmiprajapati707-jpg <urmiprajapati707@gmail.com>
2025-12-25 20:17:53 +0530

    [Urmi prajapati] : added array 1 program

--- Control-flow/src/evenodd.java
+++ Control-flow/src/evenodd.java
@@ -1,3 +1,12 @@
+import java.util.*;
 public class evenodd {
-
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        int A = sc.nextInt();
+        if(A%2==0){
+            System.out.println("Number is even");
+        }else{
+            System.out.println("Number is odd");
+        }
+    }
 }
